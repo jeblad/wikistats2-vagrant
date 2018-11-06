0d1ddf5b10f6984b3085015bd453395f7efb83d8
John Erling Blad <jeblad@gmail.com>
2018-11-06 04:26:58 +0100

    Added setup.sh for the client

--- provision/setup.sh
+++ provision/setup.sh
@@ -3,3 +3,19 @@
 echo "Provisioning virtual machine..."
 
 curl -sL https://deb.nodesource.com/setup_11.x | bash - && apt-get -y install nodejs
+
+git clone https://gerrit.wikimedia.org/r/analytics/wikistats2 /vagrant/wikistats2
+
+cd /vagrant/wikistats2
+npm install gulp
+npm install
+npm install -g gulp
+cd semantic
+gulp build
+
+sudo apt-get install libfontconfig1
+
+npm install karma-phantomjs-launcher --save-dev
+
+sudo npm install -g eslint
+sudo npm install -g eslint-plugin-jasmine
